aa063578f76777e42bcf57eed1da898319d433e6
JoshHardin <jhardin1324@gmail.com>
2024-03-23 16:53:49 -0400

    add mob command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,11 +1,10 @@
 import discord
 from discord import app_commands
-from mysql.connector import Error
 
 from cogs.utility import (validate_alphanumeric, validate_height, validate_age, 
                      validate_date, validate_text, validate_level, get_db_connection,
                      close_db_connection, active_character)
-from utils.impl.moderationImpl import (add_item_logic)
+from utils.impl.moderationImpl import (add_item_logic, add_mob_logic)
 
 class moderation(app_commands.Group):
     # Select a character from multiple characters listed under a user    
@@ -21,7 +20,27 @@ class moderation(app_commands.Group):
             "method_to_obtain": method_to_obtain      
         }
         results = add_item_logic(interaction, item_data)
-        await results
+        await interaction.response.send_message(results)
+        
+    @app_commands.command()    
+    async def add_mobs(self, interaction: discord.Interaction, mob_name:str, mob_description:str, mob_type:str, floor:str, spawn_channel:str, level:str, hp:str, str:str, defense:str, spd:str, dex:str, cha:str ):
+     # Fetch user data from the database
+        mob_data = {
+            "mob_name": mob_name,
+            "mob_description": mob_description,
+            "mob_type": mob_type,  
+            "floor": floor,
+            "spawn_channel": spawn_channel,
+            "level": level,
+            "hp": hp, 
+            "str": str,
+            "defense": defense,
+            "spd": spd, 
+            "dex": dex,
+            "cha": cha,      
+        }
+        results = add_mob_logic(interaction, mob_data)
+        await interaction.response.send_message(results) 
     
  
 
--- utils/impl/inventoryimpl.py
+++ utils/impl/inventoryimpl.py
@@ -1,6 +1,5 @@
 import discord
 from discord import app_commands
-from mysql.connector import Error
 
 from cogs.utility import (validate_alphanumeric, validate_height, validate_age, 
                      validate_date, validate_text, validate_level, get_db_connection,
