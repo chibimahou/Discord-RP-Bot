3fed121a23458fb3eccf02ba201daebad11d93e9
chibimahou <chibiwendylily@gmail.com>
2024-03-24 14:40:32 -0400

    Signed-off-by: chibimahou <chibiwendylily@gmail.com>

--- cogs/inventory.py
+++ cogs/inventory.py
@@ -1,11 +1,19 @@
 import discord
 from discord import app_commands
-from utils.impl.inventoryimpl import (add_inventory_logic)
+from utils.impl.inventoryimpl import (add_logic)
 
 class inventory(app_commands.Group):
+    
     @app_commands.command()
-    async def add_inventory(self, interaction: discord.Interaction):
-        results = add_inventory_logic(interaction, interaction.user.id)
+    @app_commands.describe(item="bronze short sword", quantity="2")
+    async def add(self, interaction: discord.Interaction, item: str, quantity: int):
+        character_data = {
+            "item": item,
+            "quantity": quantity,
+            "guild_id": interaction.guild.id,
+            "discord_tag": interaction.user.id          
+        }
+        results = add_logic(interaction, character_data)
         await results
         
 async def setup(bot):
