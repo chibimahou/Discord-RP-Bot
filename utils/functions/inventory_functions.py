d63b5ed40f3fcfb4659d87e5b60f8fc9d12cb84b
chibimahou <chibiwendylily@gmail.com>
2024-04-05 23:58:31 -0400

    Updated inventory to select specific sections of inventory

--- utils/functions/inventory_functions.py
+++ utils/functions/inventory_functions.py
@@ -23,11 +23,11 @@ async def add_item_to_inventory(character_data, item_data):
         logging.info(f"Item found: {item_document['name']}")
 
         # Check if the item already exists in the user's inventory
-        item_exists = check_item_exists_in_inventory(character_document, item_data)
+        item_exists = await update_quantity_or_check_item_exists(character_document, item_document["inventory_field"], item_data, "add")
         
         # If the iten does not exist, append it to the inventory
-        logging.info(f"Item exists: {item_exists}")
-        if not item_exists:
+        logging.info(f"Item data: {character_document['inventory'][item_document['inventory_field']]}")
+        if item_exists == False:
             # If the item does not exist, append it to the inventory
             character_document["inventory"][item_document["inventory_field"]].append({
                 "oid": item_document["_id"],
@@ -36,7 +36,7 @@ async def add_item_to_inventory(character_data, item_data):
             })
         # Update the user document with the modified inventory
         update = await db["characters"].update_one({"_id": character_document["_id"]}, 
-                                     {"$set": {"inventory.equipment": character_document["inventory"]["equipment"]}})
+                                     {"$set": {"inventory.equipment": character_document["inventory"][item_document["inventory_field"]]}})
         
         if update.modified_count < 0:
             return f"Item not added!: {item_data['name']}"
@@ -58,22 +58,28 @@ async def remove_item_from_inventory(character_data, item_data):
         character_document = await get_active_character(db, character_data)
         if not character_document:
             return f"Character not found."
+        
         item_document = await get_item(db, item_data)
         if not item_document:
             return f"Item not found."
+        
         logging.info(f"Item found: {item_document['name']}")
-
+        
         # Check if the item exists in the user's inventory
-        item_exists = check_item_exists_in_inventory(character_document, item_data)
+        item_exists = await update_quantity_or_check_item_exists(character_document, item_document["inventory_field"], item_data , "remove")
         
         logging.info(f"Item exists: {item_exists}")
         
         if item_exists:
-            # If the item exists, remove it from the inventory
-            character_document["inventory"][item_document["inventory_field"]].remove({
-                "oid": item_document["_id"],
-            })
+            # Update the user document with the modified inventory
+            update = await db["characters"].update_one({"_id": character_document["_id"]}, 
+                             {"$set": {"inventory.equipment": character_document["inventory"][item_document["inventory_field"]]}})
+        
             logging.info(f"Item removed: {item_document['name']}")
+            if update.modified_count < 0:
+                return f"Failed to remove item from inventory: {item_document['name']}"
+            return f"Item removed successfully: {item_document['name']}"
+        
         else:
             return f"Item not found in inventory."
 
@@ -82,30 +88,11 @@ async def remove_item_from_inventory(character_data, item_data):
         return f"Failed to remove item from inventory: {str(e)}"
 
     finally:
-        close_db_connection(client)
-        
-def check_item_exists_in_inventory(character_document, item_data):
-    # Initialize item_exists to False
-    item_exists = False
-    
+        await close_db_connection(client)
+
+async def update_quantity_or_check_item_exists(character_document, inventory_field, item_data, add_or_remove):
     # Iterate over each equipment_item in the character's equipment array
-    for equipment_item in character_document["inventory"]["equipment"]:
-        # Log the name of the current equipment item
-        logging.info(f"Equipment item: {equipment_item}")
-
-        # Check if the current equipment_item's name matches the item_data name
-        if equipment_item["name"] == item_data["name"]:
-            logging.info(f"Item exists in inventory: {item_data['name']}")
-            
-            # If a match is found, increase the quantity of the equipment item
-            item_exists = True
-            break
-
-    return item_exists
-
-def update_quantity(character_document, item_data, add_or_remove):
-    # Iterate over each equipment_item in the character's equipment array
-    for equipment_item in character_document["inventory"]["equipment"]:
+    for equipment_item in character_document["inventory"][inventory_field]:
         # Log the name of the current equipment item
         logging.info(f"Equipment item: {equipment_item}")
 
@@ -114,11 +101,15 @@ def update_quantity(character_document, item_data, add_or_remove):
             logging.info(f"Item exists in inventory: {item_data['name']}")
 
             # If a match is found, increase the quantity of the equipment item
+            if add_or_remove == "check":
+                break
             if add_or_remove == "add":
                 equipment_item["quantity"] += item_data["quantity"]
             else:
                 equipment_item["quantity"] -= item_data["quantity"]
                 if equipment_item["quantity"] <= 0:
-                    character_document["inventory"]["equipment"].remove(equipment_item)
-            break
-    return character_document
+                    character_document["inventory"][inventory_field].remove(equipment_item)
+                    
+            logging.info(f"Item quantity updated: {character_document['inventory'][inventory_field]}")
+            return True
+    return False
